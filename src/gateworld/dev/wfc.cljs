66498f0de23cb66d9e410f8a9e3727ce97924d01
Tom Hutchinson <git@tomisme.com>
2018-06-20 18:03:28 +0800

    WFC -- running!

--- src/gateworld/dev/wfc.cljs
+++ src/gateworld/dev/wfc.cljs
@@ -93,6 +93,7 @@
 ;;
 
 
+;; TODO get rid of these globals
 (def pattern-width 2)
 (def pattern-height 2)
 
@@ -194,7 +195,7 @@
                             (vec (repeat num-patterns true)))))))
 
 
-;; TODO freq info
+;; TODO use freq info
 (defn cell-entropy
   [cell]
   (reduce
@@ -230,12 +231,12 @@
      (let [low-num (:low-num details)
            entropy (:entropy cell)]
        (cond
-         (or (:contradiction details) (= entropy 0))
+         (or (:contradiction details)
+             (= entropy 0))
          details
 
          (< entropy 0)
          (assoc details :contradiction true)
-
 
          (or (not low-num) (< entropy low-num))
          (-> details
@@ -243,7 +244,14 @@
              (assoc :low-cells (list cell)))
 
          (= entropy low-num)
-         (update details :low-cells conj cell))))
+         (update details :low-cells conj cell)
+
+         (> entropy low-num)
+         details
+
+         :else
+         (let [_ (throw "?")]
+           details))))
    {:low-num nil
     :low-cells '()
     :contradiction false}
@@ -270,15 +278,16 @@
     (cond
       (:contradiction e-details)
       (let [_ (js/console.log "contradiction!")]
-        wave)
+        [wave nil])
 
       (empty? (:low-cells e-details))
-      (let [ _ (js/console.log "done!")]
-        wave)
+      (let [ _ (js/console.log "done!" e-details)]
+        [wave nil])
 
       :else
       (let [cell (rand-nth (:low-cells e-details))]
-        (update-in wave [(:y cell) (:x cell)] collapse-cell)))))
+        [(update-in wave [(:y cell) (:x cell)] collapse-cell)
+         [(:x cell) (:y cell)]]))))
 
 
 (defn build-neighbor-offsets
@@ -297,27 +306,26 @@
   [constraints C1 C2 offset]
   (vec (map-indexed
         (fn [P2-idx P2-valid?]
-          (if P2-valid?
+          (if (not P2-valid?)
+            false
             (reduce-kv
              (fn [P2-still-valid? P1-idx P1-valid?]
-               (if P1-valid?
-                 (if P2-still-valid?
+               (if P2-still-valid?
+                 true
+                 (if (not P1-valid?)
+                   P2-still-valid?
                    (contains? (get-in constraints [P1-idx offset])
-                              P2-idx))
-                 P2-still-valid?))
-             true
-             C1)
-            false))
+                              P2-idx))))
+             false
+             C1)))
         C2)))
 
 
 (defn propagate-next
-  [initial-wave constraints initial-frontier offsets o-w o-h]
-  (let [
-        C1-pos (peek initial-frontier)
+  [initial-wave constraints initial-frontier offsets width height]
+  (let [C1-pos (peek initial-frontier)
         C1 (get-2d initial-wave C1-pos)
-        neighbor-offsets (build-neighbor-offsets offsets o-w o-h C1-pos)]
-    ;; loop over neighbors
+        neighbor-offsets (build-neighbor-offsets offsets width height C1-pos)]
     (loop [wave initial-wave
            frontier (pop initial-frontier)
            i 0]
@@ -338,29 +346,52 @@
         [wave frontier]))))
 
 
-(defn propagate-from-pos
-  [initial-wave constraints pos offsets o-w o-h]
+(defn propagate
+  [initial-wave constraints pos offsets width height]
   (loop [wave initial-wave
          frontier #queue [pos]]
     (if (empty? frontier)
       wave
-      (let [[wave' frontier'] (propagate-next wave constraints frontier offsets o-w o-h)]
+      (let [[wave' frontier']
+            (propagate-next wave constraints frontier offsets width height)]
         (recur wave' frontier')))))
+
+
+(defn run
+  [{:keys [patterns pattern-width pattern-height width height]}]
+  (let [offsets (build-offsets pattern-width pattern-height)
+        constraints (build-overlap-index patterns offsets)
+        initial-wave (build-wave width height (count patterns))]
+    (loop [wave initial-wave]
+      (let [[obs-wave observed-pos] (observe wave)]
+        (if (= obs-wave wave)
+          wave
+          (recur (propagate obs-wave constraints observed-pos offsets width height)))))))
 
 
 ;;
 
 
-(def o-w 6)
-(def o-h 6)
-
-
 (def sample
-  [[:white :white :white :white]
-   [:white :black :black :black]
-   [:white :black :red :black]
-   [:white :black :black :black]])
+  [[:white :white :black :white :white :white]
+   [:white :black :black :black :white :white]
+   [:white :red :yellow :red :white :white]
+   [:white :black :black :black :white :white]
+   [:white :white :white :black :white :white]])
 #_(defcard sample sample)
+
+
+(defcard-rg full-render
+  (let [patterns (patterns-from-sample sample)]
+    (render-wave
+     (run {:patterns patterns
+           :pattern-width 2
+           :pattern-height 2
+           :width 20
+           :height 20})
+     patterns)))
+
+
 (defcard-rg sample-render
   (render-pattern sample))
 
@@ -381,33 +412,21 @@
   (render-overlap-index patterns overlap-index))
 
 
-(def wave (assoc-in (build-wave o-w o-h (count patterns))
-                    [0 0]
-                    [true false false false false false false]))
+#_(def wave (build-wave 5 5 (count patterns)))
 #_(defcard wave wave)
 #_(defcard-rg wave-render
     (render-wave wave patterns))
 
-(def pos [0 0])
-
-(defonce test-wave (reagent.core/atom wave))
-(defn test-render [] (render-wave @test-wave patterns))
-(defcard-rg test
-  [:div
-   [test-render]
-   [:div
-    [:button {:on-click #(swap! test-wave observe)}
-     "observe"]
-    [:button {:on-click #(swap! test-wave propagate-from-pos overlap-index pos offsets o-w o-h)}
-     "propagate"]]]
-  test-wave
-  {:history true
-   :inspect-data true})
-
-
-(defcard constrained-neighbor
-  (constrained-neighbor
-   overlap-index
-   [true false false false false false false]
-   [true true true true true true true]
-   [1 0]))
+#_(defonce test-wave (reagent.core/atom wave))
+#_(defn test-render [] (render-wave @test-wave patterns))
+#_(defcard-rg stepped-test
+    [:div
+     [test-render]
+     [:div
+      [:button {:on-click #()}
+       "observe"]
+      [:button {:on-click #()}
+       "propagate"]]]
+    test-wave
+    {:history true
+     :inspect-data true})
